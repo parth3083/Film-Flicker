e0a58b61c0be83d04fcd0d074337501ec98306bb
parth3083 <parthbrajput30@gmail.com>
2024-10-17 01:21:51 +0530

    updated

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,9 +1,6 @@
 import HeroSection from "@/components/Landing Page/HeroSection";
-import Loading from "@/components/Loading";
-import MaxWidth from "@/components/MaxWidth";
-import { IoIosArrowForward } from "react-icons/io";
+
 import React from "react";
-import Link from "next/link";
 
 function page() {
   return (
@@ -17,14 +14,7 @@ function page() {
       className="w-full h-screen  main-bg"
     >
       <div className="transparent div mask-div bg-black w-full flex items-center justify-center flex-col text-white h-full bg-opacity-70">
-        <h1 className="font-ala text-6xl font-extrabold w-[50%] ">Recommends Unlimited movies, TV shows and more</h1>
-        
-        <p className="font-ala text-xl mt-5 font-normal tracking-wider">Your next favorite is just a click away!</p>
-        <p className="font-ala text-xl mt-10 font-normal tracking-wider">Excited to explore? Sign up with your email for tailored movie suggestions!</p>
-        <Link href={'/sign-up'} className="py-2 px-4 font-ala text-2xl mt-5 rounded-md border-2 border-red-600 text-red-600 transition-all ease-in-out duration-300 hover:bg-red-600 hover:text-white flex items-center justify-center gap-2">
-          Get Started
-          <IoIosArrowForward />
-        </Link>
+        <HeroSection />
       </div>
     </div>
   );
--- client/src/components/Landing Page/HeroSection.tsx
+++ client/src/components/Landing Page/HeroSection.tsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from "react";
+import Link from "next/link";
+import { IoIosArrowForward } from "react-icons/io";
 
 function HeroSection() {
   return (
-      
-  )
+    <>
+      <h1 className="font-ala text-6xl font-extrabold w-[50%] ">
+        Recommends Unlimited movies, TV shows and more
+      </h1>
+
+      <p className="font-ala text-xl mt-5 font-normal tracking-wider">
+        Your next favorite is just a click away!
+      </p>
+      <p className="font-ala text-xl mt-10 font-normal tracking-wider">
+        Excited to explore? Sign up with your email for tailored movie
+        suggestions!
+      </p>
+      <Link
+        href={"/sign-up"}
+        className="py-2 px-4 font-ala text-2xl mt-5 rounded-md border-2 border-red-600 text-red-600 transition-all ease-in-out duration-300 hover:bg-red-600 hover:text-white flex items-center justify-center gap-2"
+      >
+        Get Started
+        <IoIosArrowForward />
+      </Link>
+    </>
+  );
 }
 
-export default HeroSection
+export default HeroSection;
